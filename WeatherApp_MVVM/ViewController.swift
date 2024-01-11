0bad3e1edcb5d8b75db385cd17ae0ffbf06599fc
Ilya Kuznetsov <kuznecov95-95@yandex.ru>
2024-01-11 17:09:51 +0300

    Initial Commit

---
+++ WeatherApp_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp_MVVM
+//
+//  Created by Илья Кузнецов on 11.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
